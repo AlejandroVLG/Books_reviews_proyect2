04d893e1880a933cdecaf6ba37e879a4fc281150
Alexlg <Alejandrolg86@gmail.com>
2022-12-20 11:58:32 +0100

    feat: isAdmin/isSuperAdmin created and added on kernel

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAdmin
+{   
+    const ROLE_ADMIN = 2;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {   
+        $userId = auth()->user()->id;
+
+        $user = User::find($userId);
+
+        $isAdmin = $user->roles->contains(self::ROLE_ADMIN);
+
+        if (!$isAdmin) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Route does not found'
+                ],
+                404
+            );
+        }
+        return $next($request);
+    }
+}
